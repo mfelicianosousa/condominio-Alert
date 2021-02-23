dab6943317a29826091248be2cafa96a031bda66
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-02-22 21:56:01 -0400

    Project condominio alert created

---
+++ backend/src/test/java/br/net/mfs/control/alert/AlertApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.mfs.control.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
